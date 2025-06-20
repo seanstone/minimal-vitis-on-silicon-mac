5195db72d28b9ebed6fc2c9afa187ed0aede73b2
En Shih <seanstone5923@gmail.com>
2025-06-20 14:22:44 +0800

    Extract installer at current dir

--- install.sh
+++ install.sh
@@ -8,11 +8,11 @@ function f_echo {
 # Extract installer
 f_echo "Extracting installer"
 chmod +x ./*.bin
-./*.bin --target /home/user/installer --noexec
+./*.bin --target ./installer --noexec
 
 # Get AuthToken by repeating the following command until it succeeds
 f_echo "Log into your Xilinx account to download the necessary files."
-while ! /home/user/installer/xsetup -b AuthTokenGen
+while ! ./installer/xsetup -b AuthTokenGen
 do
 	f_echo "Your account information seems to be wrong. Please try logging in again."
 	sleep 1
@@ -21,5 +21,5 @@ done
 # Run installer
 f_echo "You successfully logged into your account. The installation will begin now."
 f_echo "If a window pops up, simply close it to finish the installation."
-# /home/user/installer/xsetup -b ConfigGen
-/home/user/installer/xsetup -c ./install_config_${VERSION}.txt -b Install -a XilinxEULA,3rdPartyEULA
+# ./installer/xsetup -b ConfigGen
+./installer/xsetup -c ./install_config_${VERSION}.txt -b Install -a XilinxEULA,3rdPartyEULA
